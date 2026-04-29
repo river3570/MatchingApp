ce9c1d1db3721384e33d7a46729a6084a26642e4
kawa8080 <R.riverJob1590@gmail.com>
2026-04-29 10:43:43 +0000

    モデルファイルの作成

---
+++ app/models/converation.rb
@@ -0,0 +1,2 @@
+class Converation < ApplicationRecord
+end
---
+++ test/models/converation_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ConverationTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
