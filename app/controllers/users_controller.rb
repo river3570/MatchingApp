91ab462abd63078317c09911a49641d5a53469d4
kawa8080 <R.riverJob1590@gmail.com>
2026-06-03 12:18:12 +0000

    ユーザー詳細機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  
   USER_COUNT = 12
   def index
     @users = User.page(params[:page]).per(USER_COUNT)
@@ -10,5 +12,30 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'ユーザーが正常に更新されました'
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :nickname,
+      :age,
+      :gender_id,
+      :introduction,
+      :user_image,
+      hobby_ids: []
+    )
   end
 end
