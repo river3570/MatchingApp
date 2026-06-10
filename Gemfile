bd5e0cb03ce256b5696f15e644d7aa632b55db69
kawa8080 <R.riverJob1590@gmail.com>
2026-06-10 12:47:37 +0000

    チャット押下時にメッセージ部分のみ更新させる処理の実装

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem 'haml-rails'
 
 gem 'kaminari', github: 'kaminari/kaminari'
 
+gem 'rack-cors'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri windows], require: 'debug/prelude'
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,14 @@
 class MessagesController < ApplicationController
   def index
     @conversations = current_user.conversations
+    if params[:id].present?
+      user = User.find(params[:id])
+      conversation =
+        Conversation.find_by(user1: user, user2: current_user) ||
+        Conversation.find_by(user1: current_user, user2: user)
+      @messages = conversation&.messages
+    else
+      @messages = []
+    end
   end
 end
